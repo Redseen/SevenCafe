5c32846662bb1f00bb80da6f3a222f3c95022f44
root <root@raspberrypiaoyapi.(none)>
2016-01-04 16:50:08 +0900

    2つのスイッチに対応

--- doStuff.py
+++ doStuff.py
@@ -5,12 +5,20 @@ from flup.server.fcgi import WSGIServer
 import sys, urlparse
  
 GPIO.setmode(GPIO.BOARD)
+GPIO.setup(15, GPIO.OUT)
 GPIO.setup(11, GPIO.OUT)
  
 def app(environ, start_response):
 	start_response("200 OK", [("Content-Type", "text/plain"), ("Access-Control-Allow-Origin", "*")])
-	a = GPIO.input(11)
-	return [str(a)]
+	v11 = GPIO.input(11)
+	v15 = GPIO.input(15)
+
+	ret = "0"
+	if str(v11) == "1":
+		ret = "1"
+	elif str(v15) == "1":
+		ret = "2"
+	return [ret]
 
 #	i = urlparse.parse_qs(environ["QUERY_STRING"])
 #	yield (' ') # flup expects a string to be returned from this function
